a1b3ac5a8a8348fd21cdaf293528e42ef8d11e68
Lucas McCully <thecarguru2401@gmail.com>
2021-05-14 00:23:33 -0300

    Fixed Namespace

--- StatsAndAchievements.cs
+++ StatsAndAchievements.cs
@@ -1,34 +1,32 @@
 using System;
 using System.Collections.Generic;
-namespace AlienGame.Managers
+
+public class StatsAndAchievements
 {
-    public class StatsAndAchievements
+    #region Game Data
+    private static DefaultData defaultData = new DefaultData();
+    #endregion
+
+    #region Data Mutators
+    public float defaultVariableOne { get => defaultData.defaultVariableOne; set { defaultData.defaultVariableOne = value; } }
+    public float defaultVariableTwo { get => defaultData.defaultVariableTwo; set { defaultData.defaultVariableTwo = value; } }
+    #endregion
+
+    #region Returning Data
+    /// <returns>Default Data Object </returns>
+    public static DefaultData GetDefaultData() => defaultData;
+    #endregion
+
+    #region Loading Data
+    /// <summary>
+    /// Loads data from DataManager
+    /// </summary>
+    public static void LoadData<T>(T data)
     {
-        #region Game Data
-        private static DefaultData defaultData = new DefaultData();
-        #endregion
-
-        #region Data Mutators
-        public float defaultVariableOne { get => defaultData.defaultVariableOne; set { defaultData.defaultVariableOne = value; } }
-        public float defaultVariableTwo { get => defaultData.defaultVariableTwo; set { defaultData.defaultVariableTwo = value; } }
-        #endregion
-
-        #region Returning Data
-        /// <returns>Default Data Object </returns>
-        public static DefaultData GetDefaultData() => defaultData;
-        #endregion
-
-        #region Loading Data
-        /// <summary>
-        /// Loads data from DataManager
-        /// </summary>
-        public static void LoadData<T>(T data)
+        if (data is DefaultData)
         {
-            if (data is DefaultData)
-            {
-                defaultData = (DefaultData)(object)data;
-            }
+            defaultData = (DefaultData)(object)data;
         }
-        #endregion
     }
+    #endregion
 }
